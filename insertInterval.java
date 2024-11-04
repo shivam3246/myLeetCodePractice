3f0fd62016de712227062405a9b41a90f94b9ee3
shivam3246 <shivam3246@gmail.com>
2024-11-04 09:26:32 +0530

    Solved using arrays Simulation

---
+++ insertInterval.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class insertInterval {
+    public static int[][] insert(int[][] intervals, int[] newInterval) {
+       List<int[]> result = new ArrayList<>();
+       
+       //Iterate through all slots
+       for(int[] slot:intervals){
+        //if newInterval before slot inset new interval
+        if(newInterval[1]<slot[0]){
+            result.add(newInterval);
+            newInterval = slot;
+        }
+        //if slot is lesser than new Interval
+        else if(slot[1]<newInterval[0])
+        result.add(slot);
+
+        //if above conditions fail it is an overlap
+        else{
+            newInterval[0] = Math.min(newInterval[0],slot[0]);
+            newInterval[1] =Math.min(newInterval[1],slot[1]);
+
+        }
+       }
+       //insert the last newInterval]
+       result.add(newInterval);
+
+       return result.toArray(new  int[result.size()][]);
+    }
+    public static void main(String[] args) {
+        int [][]intervals = {{1,3},{6,9}}; 
+        int []newInterval ={2,5};
+        int ans[][] = new int[intervals.length][intervals[0].length];
+       ans = insert(intervals, newInterval);
+        System.out.print(Arrays.deepToString(ans));
+    }
+}
